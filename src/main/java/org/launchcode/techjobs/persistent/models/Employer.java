f8a03e0d3444b49357c5f82d76caeecf0cc46f87
Justin <amangiap15@gmail.com>
2022-11-13 14:20:38 -0600

    Entity annotations, description and location

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(max = 150)
+    private String location;
 
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer() {}
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,11 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+
+    private String description;
+    public Skill () {}
 }
