f6c61525140f7feda5ef8391e2e6f41b1fae4c26
Justin <amangiap15@gmail.com>
2022-11-13 14:11:30 -0600

    fixed implementation issues, AbstractEntity is done

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -1,12 +1,19 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank
+    @Size(max = 80)
     private String name;
 
     public int getId() {
